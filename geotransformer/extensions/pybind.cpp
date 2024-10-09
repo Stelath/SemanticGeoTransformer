d44f88f12f7719f8fdd7543e4cbb2975c2aa1429
Stelath <korte@clemson.edu>
2024-10-09 16:50:02 -0400

    Fix grid subsampling build

--- geotransformer/extensions/pybind.cpp
+++ geotransformer/extensions/pybind.cpp
@@ -2,6 +2,7 @@
 
 #include "cpu/radius_neighbors/radius_neighbors.h"
 #include "cpu/grid_subsampling/grid_subsampling.h"
+#include "cpu/grid_subsampling_with_labels/grid_subsampling_with_labels.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   // CPU extensions
@@ -14,5 +15,10 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     "grid_subsampling",
     &grid_subsampling,
     "Grid subsampling (CPU)"
+  );
+  m.def(
+    "grid_subsampling_with_labels",
+    &grid_subsampling_with_labels,
+    "Grid subsampling with labels (CPU)"
   );
 }
